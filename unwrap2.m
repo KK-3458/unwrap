4dcbb3fc0a0d737de7789b35041688f2ddfbf6d7
KK <924406004@qq.com>
2021-08-09 20:07:23 +0800

    想要试试分两步解包裹，先空间上，再时间上

--- unwrap2.m
+++ unwrap2.m
@@ -1,6 +1,6 @@
 v1='f_';
-v2='_tlow.bmp';%try
-v4='_tun.bmp';
+v2='_tm.bmp';%try
+v4='_t.bmp';
 frame=100;
 pic=zeros(360,400,100);
 kk=zeros(1,100);
@@ -15,8 +15,11 @@ for n=1:frame
     ms=int2str(n);
     name=strcat(v1,m,ms,v2);
     pic(:,:,n)=imread(name);
+    name2=strcat(v1,m,ms,v4);
+    pic2(:,:,n)=imread(name);
     kk(n)=pic(190,205,n);
 end
+
 Max=zeros(360,400);
 Min=Max;
 for i=1:360
@@ -33,110 +36,108 @@ for k=1:100
             
             C(i,j,k)=(2*pic(i,j,k)-Max(i,j)-Min(i,j))/(Max(i,j)-Min(i,j));
             wrapedphase(i,j,k)=acos(C(i,j,k));
-            %             if k~=100
-            %                 if pic(i,j,k)<pic(i,j,k+1)
-            %                     wrapedphase2(i,j,k)=2*pi-wrapedphase(i,j,k);
-            %                 else
-            %                     wrapedphase2(i,j,k)=wrapedphase(i,j,k);
-            %                 end
-            %             else
-            %                 wrapedphase2(i,j,k)=wrapedphase(i,j,k);
-            %             end
+                        if k~=100
+                            if pic(i,j,k)<pic(i,j,k+1)
+                                wrapedphase2(i,j,k)=2*pi-wrapedphase(i,j,k);
+                            else
+                                wrapedphase2(i,j,k)=wrapedphase(i,j,k);
+                            end
+                        else
+                            if pic(i,j,k)<pic(i,j,k-1)
+                            wrapedphase2(i,j,k)=wrapedphase(i,j,k);
+                            else
+                             wrapedphase2(i,j,k)=2*pi-wrapedphase(i,j,k);
+                            end
+                        end
             
         end
     end
 end
-kf=0;kb=0;numf=0;numb=0;
-for k=2:99
-    kf=0;kb=0;
-    for i=1:360
-        for j=1:400
-            if (pic(i,j,k-1)-pic(i,j,k))>0 && (pic(i,j,k)-pic(i,j,k+1))<0
-                kb=kb+1;
-            end
-            if (pic(i,j,k-1)-pic(i,j,k))<0 && (pic(i,j,k)-pic(i,j,k+1))>0
-                kf=kf+1;
-            end
-        end
-    end
-    if kb>50000  %找出的两个端点不一样的阈值
-       
-         numb=numb+1;
-        kbend(numb)=k;
-    end
-    if kf>72000
-         numf=numf+1;
-        kfend(numf)=k;
-    end
-end
-% for k=1:kfend(1) && k=kbend(1):kfend(2)
+% kf=0;kb=0;numf=0;numb=0;
+% for k=2:99
+%     kf=0;kb=0;
+%     for i=1:360
+%         for j=1:400
+%             if (pic(i,j,k-1)-pic(i,j,k))>0 && (pic(i,j,k)-pic(i,j,k+1))<0
+%                 kb=kb+1;
+%             end
+%             if (pic(i,j,k-1)-pic(i,j,k))<0 && (pic(i,j,k)-pic(i,j,k+1))>0
+%                 kf=kf+1;
+%             end
+%         end
+%     end
+%     if kb>50000  %找出的两个端点不一样的阈值
+%        
+%          numb=numb+1;
+%         kbend(numb)=k;
+%     end
+%     if kf>72000
+%          numf=numf+1;
+%         kfend(numf)=k;
+%     end
+% end
+% % for k=1:kfend(1) && k=kbend(1):kfend(2)
+% % 
+% for k=1:100
+%     for i=1:360
+%         for j=1:400
+%             if k~=100
+%                 if (((k<4 || (k>=28 && k<52) || k>=76) && (pic(i,j,k)-pic(i,j,k+1))<0))      ||    (((k>=4 && k<28) || (k>=52 && k<76)) && ((pic(i,j,k)-pic(i,j,k+1))>=0))
+%                     wrapedphase2(i,j,k)=wrapedphase(i,j,k);
+%                 else
+%                     wrapedphase2(i,j,k)=2*pi-wrapedphase(i,j,k);
+%                 end
+%             else
+%                 wrapedphase2(i,j,k)=2*pi-wrapedphase(i,j,k);
+%             end
+%         end
+%     end
+% end
+% for k=1:100  %接近PI和2PI的进行平均
+%     for i=1:360
+%         for j=1:400
+%             if i~=1 && i~=360 && j~=1 && j~=400
+%                 if abs(wrapedphase2(i,j,k)-pi)<=0.2*pi || abs(wrapedphase2(i,j,k)-2*pi)<=0.2*pi
+%                     wrapedphase2(i,j,k)=(wrapedphase2(i-1,j-1,k)+wrapedphase2(i-1,j,k)+wrapedphase2(i-1,j+1,k)+wrapedphase2(i,j-1,k)+wrapedphase2(i,j+1,k)+wrapedphase2(i+1,j-1,k)+wrapedphase2(i+1,j,k)+wrapedphase2(i+1,j+1,k))/8;
+%                 end
+%             end
+%         end
+%     end
+% end
 % 
-for k=1:100
-    for i=1:360
-        for j=1:400
-            if k~=100
-                if (((k<4 || (k>=28 && k<52) || k>=76) && (pic(i,j,k)-pic(i,j,k+1))<0))      ||    (((k>=4 && k<28) || (k>=52 && k<76)) && ((pic(i,j,k)-pic(i,j,k+1))>=0))
-                    wrapedphase2(i,j,k)=wrapedphase(i,j,k);
-                else
-                    wrapedphase2(i,j,k)=2*pi-wrapedphase(i,j,k);
-                end
-            else
-                wrapedphase2(i,j,k)=2*pi-wrapedphase(i,j,k);
-            end
-        end
-    end
-end
-for k=1:100  %接近PI和2PI的进行平均
-    for i=1:360
-        for j=1:400
-            if i~=1 && i~=360 && j~=1 && j~=400
-                if abs(wrapedphase2(i,j,k)-pi)<=0.2*pi || abs(wrapedphase2(i,j,k)-2*pi)<=0.2*pi
-                    wrapedphase2(i,j,k)=(wrapedphase2(i-1,j-1,k)+wrapedphase2(i-1,j,k)+wrapedphase2(i-1,j+1,k)+wrapedphase2(i,j-1,k)+wrapedphase2(i,j+1,k)+wrapedphase2(i+1,j-1,k)+wrapedphase2(i+1,j,k)+wrapedphase2(i+1,j+1,k))/8;
-                end
-            end
-        end
-    end
-end
 
 
 unwrapedphase(:,:,:)=unwrap(wrapedphase2,[],2);
-
 %     for i=1:360
 %         for j=1:400
 %             for k=1:100
-%                 jj(:)=unwrap(reshape(wrapedphase(i,j,:),[100,1]));
-%                 unwrapedphase(i,j,:)=jj;
+%                 unwrapedphase2(i,j,k)=uint8(unwrapedphase(i,j,k));
 %             end
 %         end
 %     end
     
   %归一化        
 for k=1:100
-    max_unwrapedphase(k)=max(reshape(unwrapedphase(:,:,k),[360*400,1]));
+   
     min_unwrapedphase(k)=min(reshape(unwrapedphase(:,:,k),[360*400,1]));
+    max_unwrapedphase(k)=max(reshape(unwrapedphase(:,:,k),[360*400,1]));
+%      unwrapedphase(:,:,k)=unwrapedphase(:,:,k)-min_unwrapedphase(k);
+%      max_unwrapedphase(k)= max_unwrapedphase(k)-min_unwrapedphase(k);
+%       min_unwrapedphase(k)= min_unwrapedphase(k)-min_unwrapedphase(k);
 end
+CC=unwrapedphase(:,:,25);
 for k=1:100
     
     for i=1:360
         for j=1:400
             normalized_unwrapedphase(i,j,k)=(unwrapedphase(i,j,k)-min_unwrapedphase(k))/(max_unwrapedphase(k)-min_unwrapedphase(k));
-        end
-    end
-end
-for k=1:100
-    max_wrapedphase(k)=max(reshape(wrapedphase2(:,:,k),[360*400,1]));
-    min_wrapedphase(k)=min(reshape(wrapedphase2(:,:,k),[360*400,1]));
-end
-for k=1:100
-    
-    for i=1:360
-        for j=1:400
-            normalized_wrapedphase2(i,j,k)=(wrapedphase2(i,j,k)-min_wrapedphase(k))/(max_wrapedphase(k)-min_wrapedphase(k));
+
         end
     end
 end
 
-for k=33:38
+
+for k=20:25
     figure(k);
     subplot(131);
     imshow(pic(:,:,k)/256);
@@ -149,24 +150,24 @@ for k=33:38
     title('解包裹');
 end
 
-kk1=reshape(wrapedphase(180,200,:),[1,100]);
-kk2=reshape(wrapedphase2(180,200,:),[1,100]);
-kk3=unwrap(kk2);
-figure(1);
-subplot(221);plot(kk1);
-title('0~\pi');
-subplot(222);plot(kk);
-title('沿时间轴100个点的信号');
-subplot(223);plot(kk2);
-title('0~2\pi');
-subplot(224);
-plot(kk3);
-title('展开相位');
+% kk1=reshape(wrapedphase(180,200,:),[1,100]);
+% kk2=reshape(wrapedphase2(180,200,:),[1,100]);
+% kk3=unwrap(kk2);
+% figure(1);
+% subplot(221);plot(kk1);
+% title('0~\pi');
+% subplot(222);plot(kk);
+% title('沿时间轴100个点的信号');
+% subplot(223);plot(kk2);
+% title('0~2\pi');
+% subplot(224);
+% plot(kk3);
+% title('展开相位');
 
-myobj= VideoWriter('myvideo2.avi');
+myobj= VideoWriter('myvideo4.avi');
 myobj.FrameRate = 5;
 open(myobj)
 for k=1:100
-    writeVideo(myobj, normalized_wrapedphase2(:,:,k));
+    writeVideo(myobj, normalized_unwrapedphase(:,:,k));
 end
 close(myobj)
